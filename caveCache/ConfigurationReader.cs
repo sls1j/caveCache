2859a602cdbce13b92fdea7ba087711f736063fc
sls1j <brian.dickey@gmail.com>
2019-01-21 10:13:40 -0700

    Added more debug messages so that I know where the config file is being read from

--- caveCache/ConfigurationReader.cs
+++ caveCache/ConfigurationReader.cs
@@ -6,43 +6,45 @@ using System.Text;
 
 namespace caveCache
 {
-    interface IConfiguration
-    {        
-        string ConnectionString { get; }
-        string ImageDirectory { get; }
-        int MaxMediaSize { get; }
-    }
+  interface IConfiguration
+  {
+    string ConnectionString { get; }
+    string ImageDirectory { get; }
+    int MaxMediaSize { get; }
+  }
 
-    class ConfigurationReader : IConfiguration
+  class ConfigurationReader : IConfiguration
+  {
+    private Dictionary<string, string> _Config;
+
+    private string GetValue(string key, string defaultValue = null)
     {
-        private Dictionary<string, string> _Config;
-
-        private string GetValue(string key, string defaultValue = null)
-        {
-            if (_Config.TryGetValue(key, out string v))
-                return v;
-            else
-                return defaultValue ?? string.Empty;
-        }
-
-        public string ConnectionString { get => GetValue("ConnectionString"); }
-        public string ImageDirectory { get => GetValue("MediaDirectory", "media"); }
-        public int MaxMediaSize
-        {
-            get
-            {
-                string v = GetValue("MaxMediaSize"); 
-                if (int.TryParse(v, out int iv))
-                    return iv;
-                else
-                    return 5 * 1024 * 1024; // 5MB is the default max size of any piece of media                 
-            }
-        }
-
-        public ConfigurationReader()
-        {
-            _Config = JsonConvert.DeserializeObject<Dictionary<string, string>>(
-                File.ReadAllText("config.json"));
-        }
+      if (_Config.TryGetValue(key, out string v))
+        return v;
+      else
+        return defaultValue ?? string.Empty;
     }
+
+    public string ConnectionString { get => GetValue("ConnectionString"); }
+    public string ImageDirectory { get => GetValue("MediaDirectory", "media"); }
+    public int MaxMediaSize
+    {
+      get
+      {
+        string v = GetValue("MaxMediaSize");
+        if (int.TryParse(v, out int iv))
+          return iv;
+        else
+          return 5 * 1024 * 1024; // 5MB is the default max size of any piece of media                 
+      }
+    }
+
+    public ConfigurationReader()
+    {
+      string fileName = Path.GetFullPath("config.json");
+      Console.WriteLine($"Reading config from '{fileName}'");
+      string configJson = File.ReadAllText(fileName);
+      _Config = JsonConvert.DeserializeObject<Dictionary<string, string>>(configJson);
+    }
+  }
 }
